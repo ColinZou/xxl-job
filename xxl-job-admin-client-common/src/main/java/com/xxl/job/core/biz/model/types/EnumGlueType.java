79b89cbf669d911647bfbac2a96558f1ddd23ca3
Kaiping Zou <kaiping.zou@thundersoft.com>
2022-05-30 16:54:32 +0800

    支持postgresql
    (cherry picked from commit c2a5eef9cfbb40cbf8695c0d30a96a4428a9cd03)
    
    xxl-job支持Postgresql

---
+++ xxl-job-admin-client-common/src/main/java/com/xxl/job/core/biz/model/types/EnumGlueType.java
@@ -0,0 +1,11 @@
+package com.xxl.job.core.biz.model.types;
+
+public enum EnumGlueType {
+    BEAN,
+    GLUE_GROOVY,
+    GLUE_SHELL,
+    GLUE_PYTHON,
+    GLUE_PHP,
+    GLUE_NODEJS,
+    GLUE_POWERSHELL
+}
