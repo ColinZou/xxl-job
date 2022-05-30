c2a5eef9cfbb40cbf8695c0d30a96a4428a9cd03
Kaiping Zou <kaiping.zou@thundersoft.com>
2022-05-30 16:54:32 +0800

    支持postgresql

--- xxl-job-admin/src/test/java/com/xxl/job/admin/dao/XxlJobRegistryDaoTest.java
+++ xxl-job-admin/src/test/java/com/xxl/job/admin/dao/XxlJobRegistryDaoTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
@@ -16,13 +19,16 @@ public class XxlJobRegistryDaoTest {
     private XxlJobRegistryDao xxlJobRegistryDao;
 
     @Test
-    public void test(){
+    public void test() {
         int ret = xxlJobRegistryDao.registryUpdate("g1", "k1", "v1", new Date());
         if (ret < 1) {
             ret = xxlJobRegistryDao.registrySave("g1", "k1", "v1", new Date());
         }
-
-        List<XxlJobRegistry> list = xxlJobRegistryDao.findAll(1, new Date());
+        Date targetTime = Date.from(LocalDateTime.now()
+            .minus(1, ChronoUnit.SECONDS)
+            .atZone(
+                ZoneId.systemDefault()).toInstant());
+        List<XxlJobRegistry> list = xxlJobRegistryDao.findAll(targetTime);
 
         int ret2 = xxlJobRegistryDao.removeDead(Arrays.asList(1));
     }
